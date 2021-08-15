ba49f216ed355d120195f8d7b65ac49725536802
Solleti Harshitha <solletiharshitha@gmail.com>
2021-08-15 21:54:22 +0530

    [Harshitha] Added -> Find Second Largest and Smallest elements in a sorted array

--- Arrays/SecondLargestandSmallestNumberSorted.js
+++ Arrays/SecondLargestandSmallestNumberSorted.js
@@ -4,9 +4,9 @@ for(let i = 1; i <= 10; i++){
     arr.push(randomValue); //Adding the values into array
 }
 console.log("The Random Elements in an array are : ");
-console.log(arr.join(','));
+console.log(arr.join(',')); //Prints the array
 let sortedArray=arr.sort();
 console.log("The Sorted array :");
-console.log(sortedArray.join(','));
+console.log(sortedArray.join(',')); //Prints the sorted array
 console.log("The Second Largest Number is : "+sortedArray[8]);
 console.log("The Second Smallest Number is : "+sortedArray[1]);
